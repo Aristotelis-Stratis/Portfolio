7657a2862e9cf7b185b61aad4ff3253111acc5ae
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-03-06 16:09:21 +0100

    burger menu implementation

--- src/app/main-content/landing-page/navbar/navbar.component.ts
+++ src/app/main-content/landing-page/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isMenuOpen: boolean = false;
+  transitionState: 'open' | 'close' | null = null;
 
+  toggleMenu(): void {
+    if (!this.isMenuOpen) {
+      this.transitionState = 'open';
+      setTimeout(() => {
+        this.isMenuOpen = !this.isMenuOpen;
+        this.transitionState = null;
+      }, 100);
+    } else {
+      this.transitionState = 'close';
+      setTimeout(() => {
+        this.isMenuOpen = !this.isMenuOpen;
+        this.transitionState = null;
+      }, 100);
+    }
+  }
 }
