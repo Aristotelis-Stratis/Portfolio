958bc88d65193238692d260e400c005ada8d745a
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-03-07 16:57:21 +0100

    about me section done fully

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
-
+  skills = [
+    { name: 'Angular', icon: 'Angular.png' },
+    { name: 'TypeScript', icon: 'TypeScript.png' },
+    { name: 'JavaScript', icon: 'JavaScript.png' },
+    { name: 'HTML', icon: 'HTML.png' },
+    { name: 'Firebase', icon: 'Firebase.png' },
+    { name: 'Git', icon: 'Git.png' },
+    { name: 'CSS', icon: 'CSS.png' },
+    { name: 'Rest-Api', icon: 'Api.png' },
+    { name: 'Scrum', icon: 'Scrum.png' },
+    { name: 'Material design', icon: 'MaterialDesign.png' }
+  ];
 }
