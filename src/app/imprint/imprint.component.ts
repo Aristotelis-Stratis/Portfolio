93e15ad9cd0f1622a15a2491e54d7109d3ae4436
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-03-13 16:17:40 +0100

    imprint

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../main-content/landing-page/navbar/navbar.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
